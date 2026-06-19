21b14a9c501f69715e8815cf35e619138eaa60b8
Rishi Katoch <rishikatoch999@gmail.com>
2026-06-19 15:40:47 +0530

    Completed Add Pet functionality

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+pets = []
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", pets=pets)
 
-@app.route("/add-pet")
+@app.route("/add-pet", methods=["GET", "POST"])
 def add_pet():
+    if request.method == "POST":
+        pet = {
+            "name": request.form.get("pet_name"),
+            "breed": request.form.get("pet_breed"),
+            "gender": request.form.get("pet_gender"),
+            "age": request.form.get("pet_age"),
+            "weight": request.form.get("pet_weight"),
+            "vaccination_status": request.form.get("pet_vaccination_status")
+        }
+
+        pets.append(pet)
+
+        return redirect(url_for("home"))
+
     return render_template("add_pet.html")
 
 if __name__ == "__main__":
