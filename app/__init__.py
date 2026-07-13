42364de7a27cd042e6a085fb1b6de51fdb3cefba
rishikatoch <rishikatoch999@gmail.com>
2026-07-13 12:44:31 +0530

    Refcator Flask app and migrate to PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pawfolio.db"
+app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://pawfolio:password@postgres:5432/pawfolio"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
