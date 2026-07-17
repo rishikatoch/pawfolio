66fa3fadb96147d9e4cc894e5e75226825b3d3e7
rishikatoch <rishikatoch999@gmail.com>
2026-07-17 23:35:12 +0530

    Run database migrations automatically during deployment

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from app import app, db
-if __name__== "__main__":
-    with app.app_context():
-        db.create_all()
+from app import app
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
-    
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
