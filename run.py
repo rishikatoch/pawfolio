11911a76e160f0a093fe93341b29d9688c16a494
rishikatoch <rishikatoch999@gmail.com>
2026-07-20 16:10:05 +0530

    Add Bandit security scan to CI

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0",  # nosec B104
+        port=5000,
+        debug=os.getenv("FLASK_DEBUG", "False").lower() == "true",
+    )
