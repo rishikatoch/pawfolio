3f0634ef725afa2e01546c32342d1c1fa82b8713
rishikatoch <rishikatoch999@gmail.com>
2026-07-20 22:58:33 +0530

    :Add Checkov Terraform security scan

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,9 +1,13 @@
 resource "aws_ecr_repository" "pawfolio" {
   name                 = "pawfolio"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
   }
 
   tags = {
