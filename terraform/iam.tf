4219e6fee7f7060ca96f9a6787fcc64d5e9c1598
rishikatoch <rishikatoch999@gmail.com>
2026-07-22 12:40:05 +0530

    Add ECR IAM permissions and deployment documentation

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -18,6 +18,11 @@ resource "aws_iam_role_policy_attachment" "ssm_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  role       = aws_iam_role.pawfolio_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 resource "aws_iam_instance_profile" "pawfolio" {
   name = "pawfolio-instance-profile"
   role = aws_iam_role.pawfolio_ec2_role.name
