a5f854f3bd1b72511a9d61fe6298df703845593a
rishikatoch <rishikatoch999@gmail.com>
2026-08-09 13:27:39 +0530

    migrate terraform state locking to s3

--- terraform/eks/backend.tf
+++ terraform/eks/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "pawfolio-terraform-state-rishi-977574653467"
-    key            = "eks/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "pawfolio-terraform-state-lock"
-    encrypt        = true
+    bucket       = "pawfolio-terraform-state-rishi-977574653467"
+    key          = "eks/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
